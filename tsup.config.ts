f6dacca8f0cf125a5a74ef26ce085cf63833b11d
Laura Ava Streisky <laura@luxploit.net>
2024-09-21 12:02:59 +0200

    replace tsup with parcel

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,13 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
 	entry: ['src/index.ts'],
 	splitting: false,
-	format: ['esm', 'cjs'],
+	format: ['esm'],
 	dts: true,
 	minify: true,
 	bundle: true,
 	clean: true,
 	outDir: 'lib',
+	outExtension(ctx) {
+		return { js: '.js', dts: 'd.ts' }
+	},
 })
